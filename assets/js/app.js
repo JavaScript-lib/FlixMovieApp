1d57400f848005f9a485f57d29c596214d752348
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-05 11:35:22 -0400

    Added the movie-detail and show-detail functions for the HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -111,10 +111,62 @@ const displayMovieDetails = async () => {
             <li><span class="text-secondary">Status:</span> ${movie.status}</li>
           </ul>
           <h4>Production Companies</h4>
-          <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join('')}</div>
+          <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
         </div>
     `;
     document.querySelector('#movie-details').appendChild(div);
+}
+const displayShowDetails = async () => {
+    const showId = window.location.search.split('=')[1];
+    const show = await fetchAPIData(`tv/${showId}`);
+    displayBackgroundImage('tv', show.backdrop_path);
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="details-top">
+          <div>
+            ${
+                show.poster_path ?
+                `<img
+                    src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+                    class="card-img-top"
+                    alt="${show.name}"/>` : 
+                `<img 
+                    src="../img/no-image.jpg" 
+                    class="card-img-top" 
+                    alt="${show.name}"/>`
+            }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>
+    `;
+    document.querySelector('#show-details').appendChild(div);
 }
 const fetchAPIData = async (endpoint) => {
     const API_KEY = 'ac428061577a1f4a80910af3ccf8610f';
@@ -180,7 +232,7 @@ const init = () => {
             displayMovieDetails();
             break;
         case '/tv-details.html':
-            console.log('Details');
+            displayShowDetails();
             break;
         case '/search.html':
             console.log('Search');
