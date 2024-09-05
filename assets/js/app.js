92c288e968d305ed6c31381cc46a00ad20650563
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-05 09:08:16 -0400

    Added the highlightActiveLink to highlight the active link

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,42 @@
 ////////////////////////////////////////////////////////////////////
 // Variables For App
 ////////////////////////////////////////////////////////////////////
-
-
+const global = {
+    currentPage: window.location.pathname,
+};
 ////////////////////////////////////////////////////////////////////
 // Functions For App
 ////////////////////////////////////////////////////////////////////
+const highlightActiveLink = () => {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link) => {
+        if(link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active');
+        }
+    });
+}
+const init = () => {
+    switch(global.currentPage) {
+        case '/':
+        case '/index.html':
+            console.log('Home');
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Details');
+            break;
+        case '/tv-details.html':
+            console.log('Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
+    highlightActiveLink();
+}
+////////////////////////////////////////////////////////////////////
+// Initializer For App
+////////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', init);
