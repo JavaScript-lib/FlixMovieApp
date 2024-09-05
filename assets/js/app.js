6087366d8c764b8827e15b384b15745835d62e15
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-05 09:56:17 -0400

    Added the images and content of the movies on the index.html page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,47 @@
 ////////////////////////////////////////////////////////////////////
 // Variables For App
 ////////////////////////////////////////////////////////////////////
-require('dotenv').config();
 const global = {
     currentPage: window.location.pathname,
 };
 ////////////////////////////////////////////////////////////////////
 // Functions For App
 ////////////////////////////////////////////////////////////////////
+const displayPopularMovies = async () => {
+    const {results} = await fetchAPIData('movie/popular');
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<div class="card">
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path ?
+                `<img
+                    src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+                    class="card-img-top"
+                    alt="${movie.title}"/>` : 
+                `<img 
+                    src="../img/no-image.jpg" 
+                    class="card-img-top" 
+                    alt="${movie.title}"/>`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        </div>`;
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
 const fetchAPIData = async (endpoint) => {
-    const API_KEY = process.env.API_KEY;
+    const API_KEY = 'ac428061577a1f4a80910af3ccf8610f';
+    const API_URL = 'https://api.themoviedb.org/3/';
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+    return data;
 }
 const highlightActiveLink = () => {
     const links = document.querySelectorAll('.nav-link');
@@ -20,13 +52,14 @@ const highlightActiveLink = () => {
     });
 }
 ////////////////////////////////////////////////////////////////////
-// Initializer For App
+// Initializer And Router For App
 ////////////////////////////////////////////////////////////////////
 const init = () => {
     switch(global.currentPage) {
         case '/':
         case '/index.html':
             console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('Shows');
