9b0217a7a3718fd498813582df584c8fa8d1c3e3
Kevin Stradtman <stradtkt22@gmail.com>
2024-09-05 11:53:25 -0400

    Added swiper functionality for the home movie page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ const global = {
     currentPage: window.location.pathname,
 };
 ////////////////////////////////////////////////////////////////////
-// Crud Functions For App
+// Crud Functions For App (GET - API)
 ////////////////////////////////////////////////////////////////////
 const displayPopularMovies = async () => {
     const {results} = await fetchAPIData('movie/popular');
@@ -216,6 +216,46 @@ const displayBackgroundImage = (type, backgroundPath) => {
         document.querySelector('#show-details').appendChild(overlay);
     }
 }
+const displaySlider = async () => {
+    const {results} = await fetchAPIData('movie/now_playing');
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+        `;
+        document.querySelector('.swiper-wrapper').appendChild(div);
+        initSwiper();
+    });
+}
+const initSwiper = () => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2
+            },
+            700: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            }
+        }
+    });
+}
 ////////////////////////////////////////////////////////////////////
 // Initializer And Router For App
 ////////////////////////////////////////////////////////////////////
@@ -223,6 +263,7 @@ const init = () => {
     switch(global.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html':
